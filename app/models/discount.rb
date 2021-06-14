68db7133b3c8164cf5edffa9a3eba7f2c28454a8
Emmy Morris <emmymorris1102@gmail.com>
2021-06-14 16:09:53 -0600

    Add new relationships and tests

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -4,5 +4,10 @@ class Discount < ApplicationRecord
                         :quantity,
                         :merchant_id
 
+ validates :percentage_discount,   numericality: true
+ validates :quantity,   numericality: true
+
   belongs_to :merchant
+  has_many :items, through: :merchant
+  has_many :invoice_items, through: :items
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -6,8 +6,13 @@ RSpec.describe Discount, type: :model do
     it { should validate_presence_of :percentage_discount }
     it { should validate_presence_of :quantity }
     it { should validate_presence_of :merchant_id }
+    it { should validate_numericality_of(:percentage_discount) }
+    it { should validate_numericality_of(:quantity) }
   end
   describe "relationships" do
     it { should belong_to :merchant }
+    it { should have_many(:items).through(:merchants) }
+    it { should have_many(:invoice_items).through(:items) }
+
   end
 end
